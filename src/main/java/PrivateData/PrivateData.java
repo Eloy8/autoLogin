0a6b12c67f9b9bf68356fffcadb84a0a6c920df6
Eloy <e.lankheet@hotmail.com>
2018-09-09 22:15:24 +0200

    Removed file

--- src/main/java/PrivateData/PrivateData.java
+++
@@ -1,9 +0,0 @@
-package PrivateData;
-
-public class PrivateData {
-
-    public static String codeSheetPathName = "C:\\Users\\Eloy\\IdeaProjects\\LoginYacht\\PrepaidCodes.xlsx";
-    public static String testSiteUrl = "https://n72.network-auth.com/splash/?mac=00%3A18%3A0A%3A83%3A2D%3AFE&real_ip=10.93.207.40&client_ip=10.93.207.40&&continue_url=http%3A%2F%2Fdetectportal.firefox.com%2Fsuccess.txt";
-    public static String geckoDriverPathName = "C:\\Users\\Eloy\\IdeaProjects\\geckodriver-v0.20.1-win64\\geckodriver.exe";
-
-}
