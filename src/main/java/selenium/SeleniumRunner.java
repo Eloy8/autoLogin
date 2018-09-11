d4c31e2b895f2df0479cc98d91132b569cf2bcc0
Unknown <e.lankheet@hotmail.com>
2018-09-11 14:16:08 +0200

    Reworked SeleniumRunner
    Added CLI for login failure.

--- src/main/java/selenium/SeleniumRunner.java
+++ src/main/java/selenium/SeleniumRunner.java
@@ -5,6 +5,8 @@ import excel.ExcelManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.Scanner;
+
 class SeleniumRunner {
 
     private static boolean successLogin = false;
@@ -12,7 +14,8 @@ class SeleniumRunner {
     private static int AmountOfCodes = ExcelManager.getAmountOfCodes();
 
     static void start() throws InterruptedException {
-        String[] codeList = new String[3];
+        String[] codeList = null;
+        codeList = new String[AmountOfCodes];
         String code = codeList[loginAttempts];
 
         System.setProperty("webdriver.gecko.driver", PrivateData.geckoDriverPathName);
@@ -51,17 +54,50 @@ class SeleniumRunner {
     }
 
     private static void quit(WebDriver driver) throws InterruptedException {
-        Thread.sleep(5000);
-        driver.quit();
+        closeDriver(driver);
+
         if (successLogin) {
-            //deleteWorking code toevoegen!
+            //deleteWorking code toevoegen en venster sluiten!
 
         } else {
-            //TODO: Een message over een internetcheck en de mogelijkheid om opnieuw te proberen!
+            System.out.println("There seems to be a problem logging in..." + "\n" + "This can be caused by internal errors or connection problems.");
+            System.out.println("Would you like to retry logging in? Type " + "retry" + "! " + "Would you like to quit this programm? Type " + "quit" + "!");
 
+            input(driver);
+        }
+    }
+
+    private static void input(WebDriver driver) throws InterruptedException {
+        Scanner input = new Scanner(System.in);
+        String seats = input.next();
+
+        if (!seats.matches("[a-zA-Z]+")) {
+            System.out.println("ERROR: Input should be alphabetical! Please type either " + "retry" + " or " + "quit" + " !");
+            input(driver);
         }
 
+        inputLoop(input, seats, driver);
+    }
 
+    private static void inputLoop(Scanner input, String seats, WebDriver driver) throws InterruptedException {
+        if (seats.toLowerCase().trim().equals("retry")) {
+            SeleniumRunner.start();
+        } else if (seats.toLowerCase().trim().equals("quit")) {
+            System.exit(0);
+        } else if (seats.toLowerCase().trim().equals("help")) {
+            quit(driver);
+        } else {
+            System.out.println("ERROR: Input does not match options! Please type either " + "retry" + " or " + "quit" + " !");
+            input(driver);
+        }
+    }
+
+    private static void closeDriver(WebDriver driver) throws InterruptedException {
+        if (loginAttempts > 0) {
+            Thread.sleep(2000);
+            driver.quit();
+        }
+        loginAttempts = 0;
     }
 
 }
